9012da69a31a4166d150543dbc5771c0b590c4e7
Kurler3 <miguelbento2000@gmail.com>
2022-11-05 10:30:01 +0000

    wdwd

--- next.config.js
+++ next.config.js
@@ -17,6 +17,10 @@ const nextConfig = {
           {
             key: 'Access-Control-Allow-Methods',
             value: 'POST'
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json'
           }
         ]
       }
