2f926b8daefae7d0bf187a29c52bf1cf4cf397f0
Kurler3 <miguelbento2000@gmail.com>
2022-11-05 08:50:32 +0000

    Adde config

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -40,7 +40,7 @@ const cors = Cors();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // csrfPrevention: true,
+    csrfPrevention: true,
     // MAKES IT SO THAT WE CAN GET THE REQUEST BODY IN THE CONTEXT IN THE RESOLVERS FUNCTION (ALLOWS TO CHECK FOR AUTH HEADERS)
     context: ({req}) => ({req}),
     introspection: true,
@@ -55,6 +55,20 @@ const startServer = server.start();
 // INIT HANDLER FUNCTION FOR THE SERVER TO CALL
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
+
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader(
+            "Access-Control-Allow-Origin",
+            "https://studio.apollographql.com"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+        );
 
         // HANDLE PRE-FLIGHT
         if(req.method === "OPTIONS") {
